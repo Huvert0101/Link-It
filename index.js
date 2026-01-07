7d273eb761b4f51429c5294824f40d0b7e168564
Huvert01 <lupi2102casrillo@gmail.com>
2026-01-06 18:09:53 -0600

    trying proxy again

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import path from 'path'
 import fs from 'fs'
+import axios from 'axios';
 import multer from 'multer';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
