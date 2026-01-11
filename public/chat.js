0e4edb9d67e1eb6fae0de46ea3459c94f5b54268
Huvert01 <lupi2102casrillo@gmail.com>
2026-01-11 17:40:56 -0600

    working on folders logic

--- public/chat.js
+++ public/chat.js
@@ -939,7 +939,14 @@ socket.on('getMessagesFol', (data)=>{
   console.log(data);
   if (!data ||data.length==0 || !data[0]?.folder?.includes("friend") && data[0]?.user == newUser) output.innerHTML = '';
   if(data?.user== newUser){output.innerHTML=''; return};
-  if(data[0].folder == currentFolder && !output.hasChildNodes()) data.forEach(el => addToDom(el))
+  if(data[0].folder == currentFolder && !output.hasChildNodes()){
+    if(!data[0].folder.includes("friend") && data[0].user == newUser){
+      data.forEach(el => addToDom(el));
+    }
+    if(data[0].folder.includes("friend") && data[0].folder == currentFolder){
+      data.forEach(el => addToDom(el));
+    }
+  }
 });
 function displayFolderMenu(folder){
   folderMenuCont = folder+"MenuCont";
