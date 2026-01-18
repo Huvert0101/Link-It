16fbaf0f9bec6d0da51840d5b8293a29bb97da37
Huvert01 <lupi2102casrillo@gmail.com>
2026-01-18 17:45:38 -0600

    trying to implement compression on backgrounds

--- public/chat.js
+++ public/chat.js
@@ -814,10 +814,11 @@ async function postBg(file){
     const optimizedUrl = dataR.secure_url.replace('/upload/', '/upload/f_avif,q_auto/');
     const imageRes = await fetch(optimizedUrl);
     const imageBlob = await imageRes.blob();
+    const avifFile = new File([imageBlob], newFileName, { type: 'image/avif' });
     const newFileName = file.name.split('.').slice(0, -1).join('.') + ".avif";
     console.log("URL del background optimizado:", optimizedUrl);
   const data = new FormData();
-  data.append("bg_src", imageBlob, newFileName);
+  data.append("bg_src", avifFile, newFileName);
   data.append("user", newUser);
   
   await axios.post('/uploadBg', data, {
